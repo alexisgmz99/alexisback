4080cad83ba322a32651027e42e25bc9cca1a32b
alexis <210013@utxicotepec.edu.mx>
2024-07-19 15:06:08 -0600

    aa

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_-w85Zyjpr_EOru5Z--G@test-utxicotepec-bd.h.aivencloud.com:21541/defaultdb"
+SQLALCHEMY_DATABASE_URL = ""
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
